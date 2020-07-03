df7f7d705941386beffa75ebd3393991bea60e13
5upermario <5upermario@users.noreply.github.com>
2020-07-03 23:04:10 +0200

    simplify site creation

--- app/Http/Actions/Site/CreateSiteAction.php
+++ app/Http/Actions/Site/CreateSiteAction.php
@@ -3,7 +3,6 @@
 namespace App\Http\Actions\Site;
 
 use App\Http\Requests\CreateSiteRequest;
-use App\Site;
 use App\User;
 
 class CreateSiteAction
@@ -13,11 +12,8 @@ class CreateSiteAction
         $request->validated();
 
         /** @var User $user */
-        $user       = $request->user();
-        $site       = new Site();
-        $site->name = $request->input('name');
-        $site->save();
-        $user->sites()->attach($site->id, ['role' => 'owner']);
+        $user = $request->user();
+        $site = $user->sites()->create(['name' => $request->input('name')], ['role' => 'owner']);
 
         return ['success' => true, 'id' => $site->id];
     }
